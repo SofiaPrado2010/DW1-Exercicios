62046465f1cd9753c8a4d21dd2c9a4b601953d91
SofiaPrado2010 <sofia.2010@alunos.utfpr.edu.br>
2026-04-08 19:39:47 -0300

    melhorei função de cálculo com validação de entrada e feedback visual para o usuário

--- Estacionamento/Estacionamento.js
+++ Estacionamento/Estacionamento.js
@@ -3,9 +3,26 @@ function Calcular(){
     let veiculo = document.getElementById("veiculo").checked;
     let cliente = document.getElementById("inputCliente").checked;
 
+    let footer = document.getElementById("footer");
+    let mensagem = document.getElementById("mensagem");
+
     let tarifa = 5 + (horas - 1 ) * 2.50;
+    if(isNaN(horas)){
+        footer.classList.add("erro");
+        mensagem.innerHTML = "As quantidade de horas deve ser informada";
+        document.getElementById("resposta").innerHTML = "-";
+        return;
+    }else{
+        footer.classList.remove("erro");
+        mensagem.innerHTML = "Cálculo realizado com sucesso";
+    }
 
     let resp = tarifa;
+    if(horas == 24){
+        resp = 60;
+    }else{
+        resp = tarifa;
+    }
     if(cliente){
         resp = resp - (resp * 5/100);
     }
