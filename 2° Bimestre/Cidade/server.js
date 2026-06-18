850da3bf547e1c07e926c78ac8797e984f928e71
SofiaPrado2010 <sofia.2010@alunos.utfpr.edu.br>
2026-06-18 10:53:17 -0300

    Fazendo algumas alterações no server.js da cidade

--- 2° Bimestre/Cidade/server.js
+++ 2° Bimestre/Cidade/server.js
@@ -100,12 +100,15 @@ app.post('/api/mensagens', async (req, res) => {
                 const query = "SELECT id_cidade, nome_cidade FROM cidade WHERE UPPER(nome_cidade) = UPPER('" + mensagemRecebida + "')";
                 const result = await pool.query(query);
 
+                console.log(result.rows);
+                console.log(result.rows.length);
+
                 if (result.rows.length > 0) {
                     return res.status(200).json({
                         status: "sucesso",
                         mensagem:
-                            `id: ${result.rows[0].id_cidade}
-                            Cidade: ${result.rows[0].nome_cidade}`
+                            `id: ${result.rows[0].id_cidade}\n` +
+                            `Cidade: ${result.rows[0].nome_cidade}`
                     });
 
                 } else {
